bde6522de6357647d313545f6a5f8aca30571523
FemiMykael <femimykael@gmail.com>
2025-05-02 22:56:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once ("php/component.php");
 require_once ("php/operation.php");
 ?>
@@ -8,8 +7,7 @@ require_once ("php/operation.php");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Books</title>
 
@@ -18,7 +16,6 @@ require_once ("php/operation.php");
 
     <!-- Custom stylesheet -->
     <link rel="stylesheet" href="style.css">
-
 </head>
 <body>
 
@@ -29,30 +26,30 @@ require_once ("php/operation.php");
         <div class="d-flex justify-content-center">
             <form action="" method="post" class="w-50">
                 <div class="pt-2">
-                    <?php inputElement("<i class='fas fa-id-badge'></i>","ID", "book_id",setID()); ?>
+                    <?php inputElement("<i class='fas fa-id-badge'></i>","ID", "book_id", setID()); ?>
                 </div>
                 <div class="pt-2">
-                    <?php inputElement("<i class='fas fa-book'></i>","Book Name", "book_name",""); ?>
+                    <?php inputElement("<i class='fas fa-book'></i>","Book Name", "book_name", ""); ?>
                 </div>
                 <div class="row pt-2">
                     <div class="col">
-                        <?php inputElement("<i class='fas fa-people-carry'></i>","Publisher", "book_publisher",""); ?>
+                        <?php inputElement("<i class='fas fa-people-carry'></i>","Publisher", "book_publisher", ""); ?>
                     </div>
                     <div class="col">
-                        <?php inputElement("<i class='fas fa-dollar-sign'></i>","Price", "book_price",""); ?>
+                        <?php inputElement("<i class='fas fa-dollar-sign'></i>","Price", "book_price", ""); ?>
                     </div>
                 </div>
                 <div class="d-flex justify-content-center">
-                        <?php buttonElement("btn-create","btn btn-success","<i class='fas fa-plus'></i>","create","data-toggle='tooltip' data-placement='bottom' title='Create'"); ?>
-                        <?php buttonElement("btn-read","btn btn-primary","<i class='fas fa-sync'></i>","read","data-toggle='tooltip' data-placement='bottom' title='Read'"); ?>
-                        <?php buttonElement("btn-update","btn btn-light border","<i class='fas fa-pen-alt'></i>","update","data-toggle='tooltip' data-placement='bottom' title='Update'"); ?>
-                        <?php buttonElement("btn-delete","btn btn-danger","<i class='fas fa-trash-alt'></i>","delete","data-toggle='tooltip' data-placement='bottom' title='Delete'"); ?>
-                        <?php deleteBtn();?>
+                    <?php buttonElement("btn-create", "btn btn-success", "<i class='fas fa-plus'></i>", "create", "data-toggle='tooltip' data-placement='bottom' title='Create'"); ?>
+                    <?php buttonElement("btn-read", "btn btn-primary", "<i class='fas fa-sync'></i>", "read", "data-toggle='tooltip' data-placement='bottom' title='Read'"); ?>
+                    <?php buttonElement("btn-update", "btn btn-light border", "<i class='fas fa-pen-alt'></i>", "update", "data-toggle='tooltip' data-placement='bottom' title='Update'"); ?>
+                    <?php buttonElement("btn-delete", "btn btn-danger", "<i class='fas fa-trash-alt'></i>", "delete", "data-toggle='tooltip' data-placement='bottom' title='Delete'"); ?>
+                    <?php deleteBtn(); ?>
                 </div>
             </form>
         </div>
 
-        <!-- Bootstrap table  -->
+        <!-- Bootstrap table -->
         <div class="d-flex table-data">
             <table class="table table-striped table-dark">
                 <thead class="thead-dark">
@@ -65,40 +62,29 @@ require_once ("php/operation.php");
                     </tr>
                 </thead>
                 <tbody id="tbody">
-                   <?php
-
-
-                   if(isset($_POST['read'])){
-                       $result = getData();
-
-                       if($result){
-
-                           while ($row = mysqli_fetch_assoc($result)){ ?>
-
-                               <tr>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['id']; ?></td>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['book_name']; ?></td>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['book_publisher']; ?></td>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo '$' . $row['book_price']; ?></td>
-                                   <td ><i class="fas fa-edit btnedit" data-id="<?php echo $row['id']; ?>"></i></td>
-                               </tr>
-
-                   <?php
-                           }
-
-                       }
-                   }
-
-
-                   ?>
+                    <?php
+                    if (isset($_POST['read'])) {
+                        $result = getData();
+                        if ($result) {
+                            while ($row = mysqli_fetch_assoc($result)) { ?>
+                                <tr>
+                                    <td data-id="<?php echo $row['id']; ?>"><?php echo $row['id']; ?></td>
+                                    <td data-id="<?php echo $row['id']; ?>"><?php echo $row['book_name']; ?></td>
+                                    <td data-id="<?php echo $row['id']; ?>"><?php echo $row['book_publisher']; ?></td>
+                                    <td data-id="<?php echo $row['id']; ?>"><?php echo '$' . $row['book_price']; ?></td>
+                                    <td><i class="fas fa-edit btnedit" data-id="<?php echo $row['id']; ?>"></i></td>
+                                </tr>
+                            <?php
+                            }
+                        }
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
 
-
     </div>
 </main>
-
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
